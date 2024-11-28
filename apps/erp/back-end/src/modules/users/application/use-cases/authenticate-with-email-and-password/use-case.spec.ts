f0d1dbcff5731e4b03cb03fc6c011b7da46dd967
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-11-28 19:02:25 -0300

    tests: finish authrntication with email and password use case

--- apps/erp/back-end/src/modules/users/application/use-cases/authenticate-with-email-and-password/use-case.spec.ts
+++ apps/erp/back-end/src/modules/users/application/use-cases/authenticate-with-email-and-password/use-case.spec.ts
@@ -1,42 +1,94 @@
-import { describe, it, beforeEach } from 'vitest';
+import { describe, it, beforeEach, expect } from 'vitest';
 
 import type { IAuthenticateUserWithEmailAndPassword } from './use-case-abstraction';
+import type { IAuthenticateUserWithEmailAndPasswordCommand } from './use-case-command';
 import type { ICryptoService } from '../../services/abstractions/crypto-service';
+import type { TCreateUser } from '@/modules/users/domain/entities/user/user';
 
 import { InMemoryUserRepository } from '@/modules/users/infrastructure/repositories/in-memory/user-repository';
 import { AuthenticateUserWithEmailAndPasswordUseCase } from './use-case';
 import { CryptoService } from '../../services/crypto-service';
+import { User } from '@/modules/users/domain/entities/user/user';
+import { generateValidUserProps } from '@/helpers/generate-valid-user-props';
+import { Password } from '@/modules/users/domain/value-objects';
 
-describe.todo(`#${AuthenticateUserWithEmailAndPasswordUseCase.name}`, _ => {
+describe(`#${AuthenticateUserWithEmailAndPasswordUseCase.name}`, _ => {
   let sut: IAuthenticateUserWithEmailAndPassword | undefined = undefined;
   let cryptoService: ICryptoService | undefined = undefined;
-  let inMemoryReadOnlyUserRepository: InMemoryUserRepository | undefined = undefined;
+  let inMemoryUserRepository: InMemoryUserRepository | undefined = undefined;
 
   // Arrange
   beforeEach(_ => {
     cryptoService = new CryptoService();
-    inMemoryReadOnlyUserRepository = new InMemoryUserRepository();
+    inMemoryUserRepository = new InMemoryUserRepository();
     sut = new AuthenticateUserWithEmailAndPasswordUseCase(
       cryptoService,
-      inMemoryReadOnlyUserRepository
+      inMemoryUserRepository
     );
   });
 
   it('should be able to return an access token for valid user', async () => {
     // Arrange
-    // const validUserProps: TCreateUser = generateValidUserProps();
-    // const user: User = User.create(validUserProps);
+    const validUserProps: TCreateUser = generateValidUserProps();
+    const [passwordHash]: [string, string] = await cryptoService!.encrypt(
+      validUserProps.password.value
+    );
+    const userPropsWithPasswordHash: TCreateUser = {
+      ...validUserProps,
+      password: Password.create({ password: passwordHash })
+    }
+    const user: User = User.create(userPropsWithPasswordHash);
 
-    // await inMemoryReadOnlyUserRepository!.createUser(user);
+    await inMemoryUserRepository!.createUser(user);
 
-    // // Act
-    // const authenticateWithEmailAndPasswordCommand: IAuthenticateUserWithEmailAndPasswordCommand = {
-    //   email: user.email.value,
-    //   password: validUserProps.password.value,
-    // };
-    // const { accessToken } = await sut!.execute(authenticateWithEmailAndPasswordCommand);
+    // Act
+    const authenticateWithEmailAndPasswordCommand: IAuthenticateUserWithEmailAndPasswordCommand = {
+      email: user.email.value,
+      password: validUserProps.password.value,
+    };
+    const { accessToken } = await sut!.execute(authenticateWithEmailAndPasswordCommand);
 
-    // // Assert
-    // expect(accessToken).toBeDefined();
+    // Assert
+    expect(accessToken).toBeDefined();
   });
+
+  it(
+    'should not be able to return an access token for invalid user password',
+    async () => {
+      // Arrange
+      const validUserProps: TCreateUser = generateValidUserProps();
+      const user: User = User.create(validUserProps);
+
+      await inMemoryUserRepository!.createUser(user);
+
+      // Act | Assert
+      const authenticateWithEmailAndPasswordCommand: IAuthenticateUserWithEmailAndPasswordCommand = {
+        email: user.email.value,
+        password: validUserProps.password.value,
+      };
+      await expect(
+        () => sut!.execute(authenticateWithEmailAndPasswordCommand)
+      ).rejects.toThrow('Invalid credentials!');
+    }
+  );
+
+  it(
+    'should not be able to return an access token for invalid user email',
+    async () => {
+      // Arrange
+      const validUserProps: TCreateUser = generateValidUserProps();
+      const user: User = User.create(validUserProps);
+
+      await inMemoryUserRepository!.createUser(user);
+
+      // Act | Assert
+      const authenticateWithEmailAndPasswordCommand: IAuthenticateUserWithEmailAndPasswordCommand = {
+        email: "invalid@email.com",
+        password: validUserProps.password.value,
+      };
+      await expect(
+        () => sut!.execute(authenticateWithEmailAndPasswordCommand)
+      ).rejects.toThrow('Invalid credentials!');
+    }
+  );
 });
