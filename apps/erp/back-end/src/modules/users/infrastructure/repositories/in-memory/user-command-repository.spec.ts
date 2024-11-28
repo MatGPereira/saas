fe2c0ae795abe75414ec33089569d329657de769
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-11-28 09:44:50 -0300

    fix(ci): install all dependencies in the root project

--- apps/erp/back-end/src/modules/users/infrastructure/repositories/in-memory/user-command-repository.spec.ts
+++ apps/erp/back-end/src/modules/users/infrastructure/repositories/in-memory/user-command-repository.spec.ts
@@ -4,21 +4,21 @@ import type { TCreateUser } from '@/modules/users/domain/entities/user/user';
 
 import { InMemoryUserCommandRepository } from './user-command-repository';
 import { User } from '@/modules/users/domain/entities/user/user';
-import { EBrazilDdd } from '@/modules/users/domain/value-objects/validators/telephone-validator-constants';
 import { generateValidUserProps } from '@/common/tests/helpers/generate-valid-user-props';
 
 describe(`#${InMemoryUserCommandRepository.name}`, _ => {
   let inMemoryUserRepository: InMemoryUserCommandRepository | undefined = undefined;
+  let validUserProps: TCreateUser | undefined = undefined;
 
   // Arrange
   beforeEach(_ => {
     inMemoryUserRepository = new InMemoryUserCommandRepository();
+    validUserProps = generateValidUserProps();
   });
 
   it('should be able to create a user', async () => {
     // Arrange
-    const validUserProps: TCreateUser = generateValidUserProps();
-    const user: User = User.create(validUserProps);
+    const user: User = User.create(validUserProps!);
 
     // Act
     const createdUserId = await inMemoryUserRepository!.createUser(user);
@@ -27,5 +27,19 @@ describe(`#${InMemoryUserCommandRepository.name}`, _ => {
     expect(createdUserId).toBeDefined();
     expect(inMemoryUserRepository!.db).toHaveLength(1);
     expect(inMemoryUserRepository!.db[0].id.toString()).toBeDefined();
+  });
+
+  it('should be able to return if an specific user exists', async () => {
+    // Arrange
+    const user: User = User.create(validUserProps!);
+    await inMemoryUserRepository!.createUser(user);
+
+    // Act
+    const hasUserWithEmail: boolean = await inMemoryUserRepository!.existUserByEmail(
+      user.email.value
+    );
+
+    // Assert
+    expect(hasUserWithEmail).toBe(true);
   });
 });
