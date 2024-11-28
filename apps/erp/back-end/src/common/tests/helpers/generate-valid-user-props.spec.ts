37797ace05ab135248500c790e6d8e7389a08bbe
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-11-28 10:08:49 -0300

    test: create test for generateValidUserProps helper function

--- apps/erp/back-end/src/common/tests/helpers/generate-valid-user-props.spec.ts
+++ apps/erp/back-end/src/common/tests/helpers/generate-valid-user-props.spec.ts
@@ -1,7 +1,30 @@
 import { expect, it, describe } from 'vitest';
 
+import type { TCreateUser } from '@/modules/users/domain/entities/user/user';
+
 import { generateValidUserProps } from './generate-valid-user-props';
 
 describe(`#${generateValidUserProps.name}`, _ => {
-  it('', _ => { });
+  it('should be able to generate valid user props', _ => {
+    // Arrange | Act
+    const validUserProps: TCreateUser = generateValidUserProps();
+
+    // Assert
+    expect(validUserProps).toEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        lastName: expect.any(String),
+        email: expect.objectContaining({
+          _value: expect.any(String)
+        }),
+        password: expect.objectContaining({
+          _value: expect.any(String)
+        }),
+        username: expect.any(String),
+        cpf: expect.objectContaining({}),
+        telephones: expect.objectContaining({}),
+        addresses: expect.objectContaining({}),
+      })
+    );
+  });
 });
